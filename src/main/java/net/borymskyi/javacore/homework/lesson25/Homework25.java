2715839ee3926723cf46057bd56329c17b079015
borymskyi <dimokskyn@gmail.com>
2022-02-16 18:22:03 +0200

    modified homework25(Полиморфизм, Кастинг)

--- src/main/java/net/borymskyi/javacore/homework/lesson25/Homework25.java
+++ src/main/java/net/borymskyi/javacore/homework/lesson25/Homework25.java
@@ -1,32 +1,54 @@
+/*
+    Homework 25. Полиморфизм / Кастинг.
+ */
 package main.java.net.borymskyi.javacore.homework.lesson25;
 
 public class Homework25 {
     public static void main(String[] args) {
-        Animal25 animal0 = new Lev25("lev");
-        Animal25 animal2 = new Pingvin25("pingvin");
-        Speakable25 [] array0 = {(Speakable25)animal0, (Speakable25)animal2};
-        if (array0 instanceof Speakable25[]) {
-            for (Speakable25 i : array0) {
-                System.out.println(((Animal25) i).name);
-                ((Animal25) i).eat25();
-                ((Animal25) i).sleep25();
-                i.speak25();
-                if (i instanceof Lev25) {
-                    ((Lev25) i).run25();
-                }
-                if (i instanceof Pingvin25) {
-                    ((Pingvin25) i).fly25();
-                }
+        Speakable25 animal0 = new Lev25("lev");
+        Speakable25 animal2 = new Pingvin25("pingvin");
+        Lev25 animal2_2 = new Lev25("lev_2");
+        Pingvin25 animal2_3 = new Pingvin25("pingvin_2");
+        Speakable25 [] array0 = {animal0, animal2, animal2_2, animal2_3};
+        for (Speakable25 i : array0) {
+            if (i instanceof Pingvin25) {
+                Pingvin25 p = (Pingvin25) i;
+                System.out.println(p.name);
+                p.eat25();
+                p.sleep25();
+                p.speak25();
+                p.fly25();
+                System.out.println();
+//                System.out.println(((Animal25) i).name);
+//                ((Animal25) i).eat25();
+//                ((Animal25) i).sleep25();
+//                i.speak25();
+//                if (i instanceof Lev25) {
+//                    ((Lev25) i).run25();
+//                }
+//                if (i instanceof Pingvin25) {
+//                    ((Pingvin25) i).fly25();
+//                }
+//                System.out.println();
+            }
+            if (i instanceof Lev25) {
+                Lev25 l = (Lev25) i;
+                System.out.println(l.name);
+                l.eat25();
+                l.sleep25();
+                l.speak25();
+                l.run25();
                 System.out.println();
             }
         }
-
         Animal25 animal3 = new Lev25("lev2");
         Animal25 animal4 = new Mechenosec25("mechenosec2");
         Animal25 animal5 = new Pingvin25("pingvin2");
-        Animal25 [] array1 = {animal3, animal4, animal5};
-        if (array1 instanceof Animal25[]) {
-            for (Animal25 i : array1) {
+        Bird25 animal6 = new Pingvin25("pingvid2_2");
+        Mammal25 animal7 = new Lev25("lev2_2");
+        Animal25 [] array1 = {animal3, animal4, animal5, animal6, animal7};
+        for (Animal25 i : array1) {
+            if (i instanceof Animal25) {
                 System.out.println(i.name);
                 i.sleep25();
                 i.eat25();
